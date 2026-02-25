87d287881d7ffdb0f9f1732151de14e9f0519230
njreadii11 <njraadii74298@gmail.com>
2026-02-25 14:08:39 +0530

    Update script.js

--- script.js
+++ script.js
@@ -20,7 +20,6 @@ function prevPage(){
     }
 }
 
-/* keyboard support */
 document.addEventListener('keydown', e => {
     if(e.key === 'ArrowRight') nextPage();
     if(e.key === 'ArrowLeft') prevPage();
