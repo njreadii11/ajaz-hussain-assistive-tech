0302cfbd9bdf31ac6cae3b2dc99e7feb575400d9
njreadii11 <njraadii74298@gmail.com>
2026-02-25 15:11:44 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,26 +1,4 @@
-let currentPage = 0;
-const pages = document.querySelectorAll('.page');
-
-function showPage(index){
-    pages.forEach(p => p.classList.remove('active'));
-    pages[index].classList.add('active');
-}
-
-function nextPage(){
-    if(currentPage < pages.length - 1){
-        currentPage++;
-        showPage(currentPage);
-    }
-}
-
-function prevPage(){
-    if(currentPage > 0){
-        currentPage--;
-        showPage(currentPage);
-    }
-}
-
-document.addEventListener('keydown', e => {
-    if(e.key === 'ArrowRight') nextPage();
-    if(e.key === 'ArrowLeft') prevPage();
-});
+window.addEventListener('click', () => {
+  const audio = new Audio('sound.mp3');
+  audio.play();
+},{once:true});
